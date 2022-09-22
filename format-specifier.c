64c6b49d691b40444b8326e993aeaf45e3642028
Ekjot Singh <workforekjot@gmail.com>
2022-09-22 22:59:55 +0530

    Completed format specifiers.

--- format-specifier.c
+++ format-specifier.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    int a =4;
+    int a = 4;
     float b = 7.333;
-    printf("Value of a is %d and value of b is %f\n", a,b);
+    printf("Value of a is %d and value of b is %f\n", a, b);
     // Now using Format Specifiers
-    /* 
+    /*
     Using whole number after decimal in number to print till certain number of digits, and using before decimals to get character spaces.
-    */ 
-    printf("Value of a is %d and value of b is %10.5f\n", a,b);
+    */
+    /* Note:
+         To take character number more than in the specifier for seeing difference in effect.
+         We can also use it in negative(-) to leave character-space after the specifier.
+    */
+    printf("Value of a is %d and value of b is %10.5f\n", a, b);
+    printf("Value of a is %d and value of b is %-10.5f\n", a, b);
     return 0;
 }
