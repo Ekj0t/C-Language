48964fa2ccca8aa3c279a3c2d972b229c894db28
Ekjot Singh <workforekjot@gmail.com>
2022-09-23 11:05:08 +0530

    use of if-else statements in C

---
+++ if-else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    printf("Enter your age:");
+    scanf("%d", &age);
+    if (age >= 18)
+    {
+        printf("You are an adult");
+    }
+    else
+    {
+        printf("You are a minor");
+    }
+
+    return 0;
+}
