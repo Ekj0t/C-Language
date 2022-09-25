559f9e22bba39eebb7037dda14554f5cdaef80e5
Ekjot Singh <workforekjot@gmail.com>
2022-09-25 20:44:01 +0530

    using of typecasting

---
+++ typecasting.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a=3;
+    float b = (float) 54/5;
+    // typecasting is used to change one data
+    // Typecasting syntax : 
+    //                      (Type) value
+    printf("The value of a is %f\n",b);
+    return 0;
+}
