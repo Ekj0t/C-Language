9dd0f8ee2270e5eb73fd14343418b91d938f666d
Ekjot Singh <workforekjot@gmail.com>
2022-09-25 20:08:26 +0530

    use of break and continue statements

---
+++ break-continue.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    int i, age;
+    for ( i = 0; i < 10; i++)
+    {
+        printf("%d Enter your age\n", i);
+        scanf("%d", &age);
+        if (age>10)
+        {
+            break;
+        }
+        if (age<10)
+        {
+            continue;
+        }
+        printf("Loll\n");
+        printf("Loll\n");
+        printf("Loll\n");
+        printf("Loll\n");
+        printf("Loll\n");
+        printf("Loll\n");
+        
+    }
+    
+    return 0;
+}
