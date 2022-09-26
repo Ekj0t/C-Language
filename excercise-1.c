57745ced87aa5e07aa6c0b090cba9002701236f2
Ekjot Singh <workforekjot@gmail.com>
2022-09-26 11:03:34 +0530

    Changed from '*' to 'x' in table

--- excercise-1.c
+++ excercise-1.c
@@ -7,7 +7,7 @@ int main()
    scanf("%d", &num);
    for (i = 1; i <= 10; i++)
    {
-      printf("%d * %d = %d\n", num, i, (num * i));
+      printf("%d x %d = %d\n", num, i, (num * i));
    }
    return 0;
 }
