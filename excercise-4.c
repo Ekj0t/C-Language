72b2009db63c2176fee20e3730f8eb5210baff84
Ekjot Singh <workforekjot@gmail.com>
2022-09-27 16:33:02 +0530

    excercise on printing fib

---
+++ excercise-4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int fib_recursive(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fib_recursive(n - 1) + fib_recursive(n - 2);
+    }
+}
+
+int fib_itreative(int n)
+{
+    int f[n+2];
+    f[0] = 0;
+    f[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        f[i] = f[i-1] + f[i-2];
+    }
+    return f[n];
+}
+int main()
+{
+    int number;
+    printf("Enter the number: ");
+    scanf("%d", &number);
+    printf("%d\n", fib_itreative(number));
+    printf("%d\n", fib_recursive(number));
+
+    return 0;
+}
