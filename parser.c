42708fe2ee679c344def0f01808811c12fd29627
Ekjot Singh <workforekjot@gmail.com>
2022-10-16 13:29:47 +0530

    Proper HTML parser

--- parser.c
+++ parser.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
+#include <string.h>
 
 // void parser
-void parser(char *p){
-    printf("The Parsed string is: ");
-    for (int i = 4; i < 22; i++)
+void parser(char *string)
+{
+    int in = 0; // Variable to see whether we are inside the tag
+    int index = 0;
+    for (int i = 0; i < strlen(string); i++)
     {
-        printf("%c", p[i]);
+        if (string[i] == '<')
+        {
+            in = 1;
+            continue;
+        }
+        else if (string[i] == '>')
+        {
+            in = 0;
+            continue;
+        }
+        if (in == 0)
+        {
+            string[index] = string[i];
+            index++;
+        }
+    } // Printed string with spaces
+    string[index] = '\0';// Ended the string
+    // Removing the spaces from the begining
+    while (string[0]== ' ')
+    {
+        // Shift the string to the left
+        for (int i = 0; i < strlen(string); i++)
+        {
+            string[i] = string[i+1];
+        }
+        
     }
+    // Removing the spaces from the end
+    while (string[strlen(string) -1]==' ')
+    {
+        string[strlen(string) -1] ='\0';
+    }
+    
     
 }
 
@@ -18,8 +52,9 @@ int main()
         Output:
         this is a heading
      */
-    char string[] = "<h1> this is a heading </h1>";
+    char string[] = "<h1>   this is a heading    </h1>";
     printf("UnParsed String: %s\n", string);
     parser(string);
+    printf("The Parsed string is ~~%s~~", string);
     return 0;
 }
