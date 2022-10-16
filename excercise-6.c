644f3f503839500edf35e6dd8cdb2350ea2f94f8
Ekjot Singh <workforekjot@gmail.com>
2022-10-16 12:18:33 +0530

    Main solution for array reversing excercise

--- excercise-6.c
+++ excercise-6.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 
-int arrayRev(int *array)
-{
-    for (int i = 6; i >= 0; i--)
+// Not best solution
+// int arrayRev(int *array)
+// {
+//     for (int i = 6; i >= 0; i--)
+//     {
+//         printf("%c", array[i]);
+//     }
+//     return 0;
+// }
+int arrayRev(int arr[]){
+
+    int temp;
+    for (int i = 0; i < 7/2; i++)
     {
-        printf("%c", array[i]);
+        // Swap item arr[i] with arr[6-i]
+        temp = arr[i];
+        arr[i] = arr[6-i];
+        arr[6-i] = temp;
     }
-    return 0;
+    
+
 }
 
 int main()
 {
-    int string[] = {'1', '2', '3', '4', '5', '6', '7', '\0'};
-    int i = 0;
-    while (string[i] != '\0')
+    int arr[] = {1,2,3,4,5,6,67};
+    printf("Before reversing the array\n");
+    for (int i = 0; i < 7; i++)
     {
-        printf("%c", string[i]);
-        i++;
+        printf("The value of element %d is %d\n", i, arr[i]);
     }
-    printf("\n");
-    arrayRev(string);
+    arrayRev(arr);
+    printf("After reversing the array:\n");
+    for (int i = 0; i < 7; i++)
+    {
+    printf("The value of element %d is %d\n", i, arr[i]);
+        
+    }
+    
+    
 
     return 0;
 }
