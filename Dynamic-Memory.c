647b7c72a45ce9b4f090d307b3998fbd14a530dd
Ekjot Singh <workforekjot@gmail.com>
2022-10-17 23:11:26 +0530

    Size command for more information of program

---
+++ Dynamic-Memory.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    /*Use size command to know about the information of the program
+    Example:
+    Input: size(Command) .\Dynamic-Memory.exe(Filename)
+    text     data     bss    dec      hex filename >> (Data in Bytes)
+    14344    1532     112   15988    3e74 .\Dynamic-Memory.exe
+    */
+    return 0;
+}
