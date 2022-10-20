e6ebfc96fe6ebd5b942ab76589111197b3504add
Ekjot Singh <workforekjot@gmail.com>
2022-10-20 22:16:40 +0530

    working with multiple c files in storage classes

---
+++ temp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int func(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+int sum = 100;
+// Decleration - Telling compiler about the variable and no space reserved.
+// Definition - Decleration + space reservation.
