242c61af72ac9ee4f469d341041103018212582b
Ekjot Singh <workforekjot@gmail.com>
2022-10-22 16:25:38 +0530

    initial setup for excercise

---
+++ excercise-7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Create Rock, Paper & Scissors Game
+//  Player 1: rock
+//  Player 2 (computer): scissors -->player 1 gets 1 point
+
+// rock vs scissors - rock wins
+// paper vs scissors - scissors wins
+// paper vs rock - paper wins
+// Write a C program to allow user to play this game three times with computer. Log the scores of computer and the player. Display the name of the winner at the end
+// Notes: You have to display name of the player during the game. Take users name as an input from the user.
+int main()
+{
+
+    return 0;
+}
